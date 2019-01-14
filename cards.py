e9f247f349ba9d3f5fd60a3ce13c9aece048fdee
Iain Goh <iaingoh@gmail.com>
2019-01-14 21:14:07 +0800

    Fifth commit: removed several testing files from the main directory; wrote code that looked for bad cards, cards that did not have the playerClass attribute and delete those cards from the collection; running the code now would not encounter key error because all cards have the attribute of interest, playerClass

--- cards.py
+++ cards.py
@@ -3,32 +3,39 @@ import random
 
 with open('response.json') as f:
     data = json.load(f)
+    print("There are " + str(len(data)) + " cards in the collection.")
 
-# Append the first card to the deck
-# Index the playerClass
-# The following cards will follow the index
-flag = True
+# Remove faulty cards where the key playerClass does not exist.
+faulty_cards = []
+for card in data:
+    if 'playerClass' not in card:
+        faulty_cards.append(card)
+        index = data.index(card)
+        del data[index]
+print("The conditional found " + str(len(faulty_cards)) + " faulty cards.")
+print("There are now " + str(len(data)) + " cards in the collection.")
+
+# 1) Append the first card to the deck
+# 2) Set the playerClass to variable
+# 3) The following cards will filter according to that variable
 deck = []
-while flag:
+while len(deck) < 30:
     card = random.choice(data)
     if not deck:
         deck.append(card)
         playerClass = card['playerClass']
         continue
         print(card)
-    elif len(deck) < 30:
-        if card['playerClass'] == playerClass or card['playerClass'] == 'Neutral':
-            deck.append(card)
-    else:
-        flag = False
+    elif card['playerClass'] == playerClass or card['playerClass'] == 'Neutral':
+        deck.append(card)
 
 for card in deck:
     print(card['playerClass'])
 
 print(len(deck))
-# Append the first card to the deck
-# Index the playerClass
-# The following cards will follow the index
+
+
+
         
         
     
